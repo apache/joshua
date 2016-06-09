44a50d8503606914897f9ea471dbf69195ce9c66
Tommaso Teofili <tommaso@apache.org>
2016-06-09 11:39:34 +0200

    JOSHUA-248 - fixed AL headers

--- src/main/java/org/apache/joshua/subsample/SubsamplerCLI.java
+++ src/main/java/org/apache/joshua/subsample/SubsamplerCLI.java
@@ -1,9 +1,20 @@
 /*
- * This file uses code from the edu.umd.clip.mt.subsample.Subsampler class from the University of
- * Maryland's jmtTools project (in conjunction with the umd-hadoop-mt-0.01 project). That project is
- * released under the terms of the Apache License 2.0, but with special permission for the Joshua
- * Machine Translation System to release modifications under the LGPL version 2.1. LGPL version 3
- * requires no special permission since it is compatible with Apache License 2.0
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.joshua.subsample;
 
