2b570d2b6ca7ad54e1cd6ed09fec58919efc59ce
Tommaso Teofili <tommaso@apache.org>
2016-08-18 10:00:54 +0200

    JOSHUA-291 - added missing import in StructuredTranslation

--- src/main/java/org/apache/joshua/decoder/StructuredTranslation.java
+++ src/main/java/org/apache/joshua/decoder/StructuredTranslation.java
@@ -21,6 +21,7 @@ package org.apache.joshua.decoder;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.joshua.decoder.io.DeNormalize;
 import org.apache.joshua.decoder.segment_file.Sentence;
 import org.apache.joshua.decoder.segment_file.Token;
 import org.apache.joshua.util.FormatUtils;
