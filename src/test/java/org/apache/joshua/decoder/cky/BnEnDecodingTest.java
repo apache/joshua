74d800657377a05ded552cb5b2b26c46a5b6bc52
Michael A. Hedderich <michael-aloys@users.noreply.github.com>
2016-09-14 14:32:35 +0200

    Moved regression test decoder/do-not-crash to unit test and refactored code to reuse utility test functions.

--- src/test/java/org/apache/joshua/decoder/cky/BnEnDecodingTest.java
+++ src/test/java/org/apache/joshua/decoder/cky/BnEnDecodingTest.java
@@ -18,26 +18,19 @@
  */
 package org.apache.joshua.decoder.cky;
 
+import static org.apache.joshua.decoder.cky.TestUtil.decodeList;
+import static org.apache.joshua.decoder.cky.TestUtil.loadStringsFromFile;
 import static org.testng.Assert.assertEquals;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.apache.joshua.decoder.Decoder;
 import org.apache.joshua.decoder.JoshuaConfiguration;
-import org.apache.joshua.decoder.segment_file.Sentence;
 import org.apache.joshua.util.io.KenLmTestUtil;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-public class CKYDecodingTest {
-
-	private final static String N_BEST_SEPARATOR = "\n";
+public class BnEnDecodingTest {
 
 	private JoshuaConfiguration joshuaConfig;
 	private Decoder decoder;
@@ -53,99 +46,76 @@ public class CKYDecodingTest {
 	@Test
 	public void givenBnEnInput_whenPhraseDecoding_thenScoreAndTranslationCorrect() throws Exception {
 		// Given
-		List<String> inputStrings = loadSentencesFromFile("src/test/resources/bn-en/hiero/input.bn");
+		List<String> inputStrings = loadStringsFromFile("src/test/resources/bn-en/hiero/input.bn");
 
 		// When
 		configureDecoder("src/test/resources/bn-en/hiero/joshua.config");
-		List<String> decodedStrings = decodeList(inputStrings);
+		List<String> decodedStrings = decodeList(inputStrings, decoder, joshuaConfig);
 
 		// Then
-		List<String> goldStrings = loadSentencesFromFile("src/test/resources/bn-en/hiero/output.gold");
+		List<String> goldStrings = loadStringsFromFile("src/test/resources/bn-en/hiero/output.gold");
 		assertEquals(decodedStrings, goldStrings);
 	}
 
 	@Test
 	public void givenBnEnInput_whenPhraseDecodingWithBerkeleyLM_thenScoreAndTranslationCorrect() throws Exception {
 		// Given
-		List<String> inputStrings = loadSentencesFromFile("src/test/resources/bn-en/hiero/input.bn");
+		List<String> inputStrings = loadStringsFromFile("src/test/resources/bn-en/hiero/input.bn");
 
 		// When
 		configureDecoder("src/test/resources/bn-en/hiero/joshua-berkeleylm.config");
-		List<String> decodedStrings = decodeList(inputStrings);
+		List<String> decodedStrings = decodeList(inputStrings, decoder, joshuaConfig);
 
 		// Then
-		List<String> goldStrings = loadSentencesFromFile("src/test/resources/bn-en/hiero/output-berkeleylm.gold");
+		List<String> goldStrings = loadStringsFromFile("src/test/resources/bn-en/hiero/output-berkeleylm.gold");
 		assertEquals(decodedStrings, goldStrings);
 	}
 
 	@Test
 	public void givenBnEnInput_whenPhraseDecodingWithClassLM_thenScoreAndTranslationCorrect() throws Exception {
 		// Given
-		List<String> inputStrings = loadSentencesFromFile("src/test/resources/bn-en/hiero/input.bn");
+		List<String> inputStrings = loadStringsFromFile("src/test/resources/bn-en/hiero/input.bn");
 
 		// When
 		configureDecoder("src/test/resources/bn-en/hiero/joshua-classlm.config");
-		List<String> decodedStrings = decodeList(inputStrings);
+		List<String> decodedStrings = decodeList(inputStrings, decoder, joshuaConfig);
 
 		// Then
-		List<String> goldStrings = loadSentencesFromFile("src/test/resources/bn-en/hiero/output-classlm.gold");
+		List<String> goldStrings = loadStringsFromFile("src/test/resources/bn-en/hiero/output-classlm.gold");
 		assertEquals(decodedStrings, goldStrings);
 	}
 	
 	@Test
 	public void givenBnEnInput_whenPhraseDecodingWithPackedGrammar_thenScoreAndTranslationCorrect() throws Exception {
 		// Given
-		List<String> inputStrings = loadSentencesFromFile("src/test/resources/bn-en/packed/input.bn");
+		List<String> inputStrings = loadStringsFromFile("src/test/resources/bn-en/packed/input.bn");
 
 		// When
 		configureDecoder("src/test/resources/bn-en/packed/joshua.config");
-		List<String> decodedStrings = decodeList(inputStrings);
+		List<String> decodedStrings = decodeList(inputStrings, decoder, joshuaConfig);
 
 		// Then
-		List<String> goldStrings = loadSentencesFromFile("src/test/resources/bn-en/packed/output.gold");
+		List<String> goldStrings = loadStringsFromFile("src/test/resources/bn-en/packed/output.gold");
 		assertEquals(decodedStrings, goldStrings);
 	}
 	
 	@Test
 	public void givenBnEnInput_whenPhraseDecodingWithSAMT_thenScoreAndTranslationCorrect() throws Exception {
 		// Given
-		List<String> inputStrings = loadSentencesFromFile("src/test/resources/bn-en/samt/input.bn");
+		List<String> inputStrings = loadStringsFromFile("src/test/resources/bn-en/samt/input.bn");
 
 		// When
 		configureDecoder("src/test/resources/bn-en/samt/joshua.config");
-		List<String> decodedStrings = decodeList(inputStrings);
+		List<String> decodedStrings = decodeList(inputStrings, decoder, joshuaConfig);
 
 		// Then
-		List<String> goldStrings = loadSentencesFromFile("src/test/resources/bn-en/samt/output.gold");
+		List<String> goldStrings = loadStringsFromFile("src/test/resources/bn-en/samt/output.gold");
 		assertEquals(decodedStrings, goldStrings);
 	}
-
-	private static List<String> loadSentencesFromFile(String pathToFile) throws IOException {
-		List<String> inputLines = Files.lines(Paths.get(pathToFile)).collect(Collectors.toList());
-		return inputLines;
-	}
-
-	private void configureDecoder(String pathToConfig) throws Exception {
+	
+	public void configureDecoder(String pathToConfig) throws Exception {
 		joshuaConfig = new JoshuaConfiguration();
 		joshuaConfig.readConfigFile(pathToConfig);
 		KenLmTestUtil.Guard(() -> decoder = new Decoder(joshuaConfig, ""));
-	}
-	
-	/**
-	 * @param inputStrings A list of strings that should be decoded
-	 * @return A list of decoded strings. If the decoder produces
-	 * an n-best list, then each translation of the n-best list
-	 * has its own entry in the returned list.
-	 */
-	private List<String> decodeList(List<String> inputStrings) {
-		final List<String> decodedStrings = new ArrayList<>();
-
-		for (String inputString : inputStrings) {
-			final Sentence sentence = new Sentence(inputString, 0, joshuaConfig);
-			final String[] nBestList = decoder.decode(sentence).toString().split(N_BEST_SEPARATOR);
-			decodedStrings.addAll(Arrays.asList(nBestList));
-		}
-
-		return decodedStrings;
 	}
 }
