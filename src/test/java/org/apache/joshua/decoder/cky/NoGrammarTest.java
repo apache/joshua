74e6c0045a1eb64b5f02eca68027c7bb681d99c5
Michael A. Hedderich <michael-aloys@users.noreply.github.com>
2016-09-14 15:55:18 +0200

    Moved regression test decoder/left-state to unit test.

--- src/test/java/org/apache/joshua/decoder/cky/NoGrammarTest.java
+++ src/test/java/org/apache/joshua/decoder/cky/NoGrammarTest.java
@@ -23,7 +23,6 @@ import static org.testng.Assert.assertEquals;
 
 import org.apache.joshua.decoder.Decoder;
 import org.apache.joshua.decoder.JoshuaConfiguration;
-import org.apache.joshua.util.io.KenLmTestUtil;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
