e60c7796742381d91e7438c30736ea5adcaed25b
Matt Post <post@cs.jhu.edu>
2016-09-15 17:12:25 +0200

    Updated test for feature ordering

--- joshua-core/src/test/java/org/apache/joshua/decoder/phrase/decode/PhraseDecodingTest.java
+++ joshua-core/src/test/java/org/apache/joshua/decoder/phrase/decode/PhraseDecodingTest.java
@@ -38,9 +38,7 @@ public class PhraseDecodingTest {
 
   private static final String CONFIG = "src/test/resources/phrase_decoder/config";
   private static final String INPUT = "una estrategia republicana para obstaculizar la reelección de Obama";
-
-
-  private static final String OUTPUT = "0 ||| a strategy republican to hinder reelection Obama ||| Distortion=0.000000 WordPenalty=-3.040061 PhrasePenalty=5.000000 pt_0=-9.702445 pt_1=-10.799793 pt_2=-7.542729 pt_3=-8.555386 lm_0=-19.116861 ||| -7.496";
+  private static final String OUTPUT = "0 ||| a strategy republican to hinder reelection Obama ||| pt_3=-8.555386 pt_2=-7.542729 pt_1=-10.799793 pt_0=-9.702445 lm_0=-19.116861 WordPenalty=-3.040061 PhrasePenalty=5.000000 Distortion=0.000000 ||| -7.496"; 
   private static final String OUTPUT_WITH_ALIGNMENTS = "0 ||| a strategy |0-1| republican |2-2| to hinder |3-4| reelection |5-6| Obama |7-8| ||| Distortion=0.000000 WordPenalty=-3.040061 PhrasePenalty=5.000000 pt_0=-9.702445 pt_1=-10.799793 pt_2=-7.542729 pt_3=-8.555386 lm_0=-19.116861 ||| -7.496";
   
   private JoshuaConfiguration joshuaConfig = null;
@@ -66,6 +64,12 @@ public class PhraseDecodingTest {
     assertEquals(translation, gold);
   }
   
+  /**
+   * Phrase alignment output is currently not available, has been removed until we refactor the
+   * output (MJP, Sept. 2016)
+   * 
+   * @throws IOException
+   */
   @Test(enabled = false)
   public void givenInput_whenPhraseDecodingWithAlignments_thenOutputHasAlignments() throws IOException {
     final String translation = decode(INPUT).toString().trim();
