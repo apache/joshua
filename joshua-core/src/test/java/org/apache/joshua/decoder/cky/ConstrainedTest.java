8f47187e4a5d8d4ea5940165b88eec1d7615610b
Matt Post <post@cs.jhu.edu>
2016-09-17 23:24:57 +0200

    fixed constrained decoding test
    I'm having some difficulty debugging errors when, for example, the config file location is given incorrectly and therefore isn't loaded. This seems to happen silently without any informative error. Just mentioning this to be sure it's on the docket.

--- joshua-core/src/test/java/org/apache/joshua/decoder/cky/ConstrainedTest.java
+++ joshua-core/src/test/java/org/apache/joshua/decoder/cky/ConstrainedTest.java
@@ -1,23 +1,20 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.apache.joshua.decoder.cky;
 
+import static com.typesafe.config.ConfigFactory.parseResources;
 import static org.apache.joshua.decoder.cky.TestUtil.decodeList;
 import static org.apache.joshua.decoder.cky.TestUtil.loadStringsFromFile;
 import static org.testng.Assert.assertEquals;
@@ -29,36 +26,35 @@ import org.apache.joshua.util.io.KenLmTestUtil;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
+import com.typesafe.config.Config;
+
 public class ConstrainedTest {
 
-	private JoshuaConfiguration joshuaConfig;
-	private Decoder decoder;
+  private Decoder decoder;
 
-	@AfterMethod
-	public void tearDown() throws Exception {
-		if(decoder != null) {
-			decoder.cleanUp();
-			decoder = null;
-		}
-	}
+  @AfterMethod
+  public void tearDown() throws Exception {
+    if (decoder != null) {
+      decoder.cleanUp();
+      decoder = null;
+    }
+  }
 
-	@Test
-	public void givenInput_whenConstrainedDecoding_thenScoreAndTranslationCorrect() throws Exception {
-		// Given
-		List<String> inputStrings = loadStringsFromFile("src/test/resources/decoder/constrained/input.bn");
+  @Test
+  public void givenInput_whenConstrainedDecoding_thenScoreAndTranslationCorrect() throws Exception {
+    // Given
+    List<String> inputStrings = loadStringsFromFile(this.getClass().getResource("ConstrainedTest.in").getFile());
 
-		// When
-		configureDecoder("src/test/resources/decoder/constrained/joshua.config");
-		List<String> decodedStrings = decodeList(inputStrings, decoder, joshuaConfig);
+    // When
+    Config config = parseResources(this.getClass(), "ConstrainedTest.conf")
+        .withFallback(Decoder.getDefaultFlags());
+    decoder = new Decoder(config);
+    
+    List<String> decodedStrings = decodeList(inputStrings, decoder);
 
-		// Then
-		List<String> goldStrings = loadStringsFromFile("src/test/resources/decoder/constrained/output.gold");
-		assertEquals(decodedStrings, goldStrings);
-	}
-	
-	public void configureDecoder(String pathToConfig) throws Exception {
-		joshuaConfig = new JoshuaConfiguration();
-		joshuaConfig.readConfigFile(pathToConfig);
-		KenLmTestUtil.Guard(() -> decoder = new Decoder(joshuaConfig));
-	}
+    // Then
+    List<String> goldStrings = loadStringsFromFile(this.getClass().getResource("ConstrainedTest.gold").getFile());
+
+    assertEquals(decodedStrings, goldStrings);
+  }
 }
