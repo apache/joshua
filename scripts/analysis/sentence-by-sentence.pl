4d3e86b2758517eebefe98d9a32c8d7463116ba6
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2016-11-11 02:04:38 -0800

    JOSHUA-323 Joshua 6.1 Release Management

--- scripts/analysis/sentence-by-sentence.pl
+++ scripts/analysis/sentence-by-sentence.pl
@@ -1,4 +1,18 @@
 #!/usr/bin/perl -w
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 #sentence-by-sentence: take in a system output, with any number of factors, and a reference translation, also maybe with factors, and show each sentence and its errors
 #usage: sentence-by-sentence SYSOUT [REFERENCE]+ > sentences.html
